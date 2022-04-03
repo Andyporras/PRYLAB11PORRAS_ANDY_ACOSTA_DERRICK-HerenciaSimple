c8add049d046366773a47aca8419a0555a75ce4a
Andy Porras <andyporras@estudiantec.cr>
2022-04-03 17:26:50 -0600

    arreglo del constructor Factura

--- logicadenegocio/Factura.java
+++ logicadenegocio/Factura.java
@@ -1,5 +1,6 @@
 package logicadenegocio;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -12,7 +13,7 @@ import javax.sound.sampled.Line;
  * @version 03/04/2022
  */
 public class Factura {
-  private int catidadFactura;
+  private static int catidadFactura;
   private String nombre;
   private String cedulaJuridica;
   private String direccion;
@@ -20,25 +21,80 @@ public class Factura {
   private Date fechaFactura;
   private Date horaFactura;
   private int numeroFactura;
+  private Mensaje anuncio;
   private ArrayList<LineaDetalle> lineaDetalle;
 
-  public Factura(String pNombre, String pCedulaJuridica, String pDireccion, int pTelefono, Date pFechaFactura,
-      Date pHoraFactura, int pNumeroFactura, Empleado pCajero, Empleado pVendedor) {
+  public Factura(String pNombre, String pCedulaJuridica, String pDireccion, int pTelefono,
+      Empleado pCajero, Empleado pVendedor) {
     lineaDetalle = new ArrayList<LineaDetalle>();
     nombre = pNombre;
     cedulaJuridica = pCedulaJuridica;
     direccion = pDireccion;
     telefono = pTelefono;
-    fechaFactura = pFechaFactura;
-    horaFactura = pHoraFactura;
-    numeroFactura = pNumeroFactura;
+    setFechaFactura();
+    setHoraFactura();
+    catidadFactura++;
+    numeroFactura = catidadFactura;
+
   }
-  public double calcularSubTotal(){
-    double subTotal=0;
-    for(LineaDetalle linea: lineaDetalle){
-      subTotal+=linea.getPrecio();
+
+  public void setHoraFactura() {
+    horaFactura = new Date();
+  }
+
+  public String getHoraFactura() {
+    SimpleDateFormat mascara = new SimpleDateFormat("HH:mm:ss");
+    return mascara.format(horaFactura);
+  }
+
+  public void setFechaFactura() {
+    fechaFactura = new Date();
+  }
+
+  public double calcularSubTotal() {
+    double subTotal = 0;
+    for (LineaDetalle linea : lineaDetalle) {
+      subTotal += linea.getPrecio();
     }
     return subTotal;
+  }
+
+  public double calcularImpuestoVenta() {
+    double impuestoVenta = 0;
+    for (LineaDetalle linea : lineaDetalle) {
+      impuestoVenta += linea.getPrecio() * 0.13;
+    }
+    return impuestoVenta;
+  }
+
+  public double calcularTotal() {
+    double total = 0;
+    total += calcularSubTotal() + calcularImpuestoVenta();
+    return total;
+  }
+
+  public void agregarAnuncio(Mensaje pAnuncio) {
+    anuncio = pAnuncio;
+  }
+
+  public String getFechar() {
+    SimpleDateFormat mascara = new SimpleDateFormat("dd/MM/yy");
+    return mascara.format(fechaFactura);
+  }
+
+  public String toString() {
+    String msj = "";
+    msj += "Nombre: " + nombre + "\n";
+    msj += "Cedula Juridica: " + cedulaJuridica + "\n";
+    msj += "Direccion: " + direccion + "\n";
+    msj += "Telefono: " + telefono + "\n";
+    msj += "Fecha Factura: " + getFechar() + "\n";
+    msj += "Hora Factura: " + horaFactura + "\n";
+    msj += "Numero Factura: " + numeroFactura + "\n";
+    msj += "SubTotal: " + calcularSubTotal() + "\n";
+    msj += "Impuesto Venta: " + calcularImpuestoVenta() + "\n";
+    msj += "Total: " + calcularTotal() + "\n";
+    return msj;
   }
 
 }
--- logicadenegocio/FacturaCredito.java
+++ logicadenegocio/FacturaCredito.java
@@ -2,19 +2,34 @@ package logicadenegocio;
 
 /**
  * 
- * @author 
+ * @author
  * @version
  */
-public class FacturaCredito{
+public class FacturaCredito extends Factura {
+
   private int cantidadMeses;
   private double interesMensual;
   private double cuotaMensual;
   private double subTotal;
-  public FacturaCredito( String pNombre, String pCedulaJuridica, String pDirrecion, int pTelefono, Empleado pCajero, Empleado pVendedor, int pCantidadMeses, double pInteresMensual, double pCuotaMensual, double pSubTotal){
-    cantidadMeses=pCantidadMeses;
-    interesMensual=pInteresMensual;
-    cuotaMensual=pCuotaMensual;
-    subTotal=pSubTotal;
+
+  FacturaCredito(String pNombre, String pCedulaJuridica, String pDireccion, int pTelefono, Empleado pCajero,
+      Empleado pVendedor, int pCantidadMeses, double pInteresMensual,
+      double pCuotaMensual) {
+    super(pNombre, pCedulaJuridica, pDireccion, pTelefono, pCajero, pVendedor);
+    cantidadMeses = pCantidadMeses;
+    interesMensual = pInteresMensual;
+    cuotaMensual = pCuotaMensual;
+    subTotal = super.calcularSubTotal();
   }
- 
+
+  public String toString() {
+    String msj = "";
+    msj += super.toString();
+    msj += "Cantidad de meses: " + cantidadMeses + "\n";
+    msj += "Interes mensual: " + interesMensual + "\n";
+    msj += "Cuota mensual: " + cuotaMensual + "\n";
+    msj += "Subtotal: " + subTotal + "\n";
+    return msj;
+  }
+
 }
